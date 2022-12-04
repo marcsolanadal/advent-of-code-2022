03f9ed41a0b9d5b20fff63a456af199b46c64b03
Marc Solà Nadal <mail@marcsolanadal.com>
2022-12-04 22:04:44 +0100

    Day 4 common puzzle input

--- src/day-4/main.js
+++ src/day-4/main.js
@@ -2,7 +2,7 @@ const { open } = require("node:fs/promises");
 const { solve } = require("./part1/part1");
 
 async function readPuzzleInput(cb) {
-  const file = await open("./part1/puzzle-input.txt");
+  const file = await open("./puzzle-input.txt");
   let overlapCount = 0;
 
   for await (const line of file.readLines()) {
